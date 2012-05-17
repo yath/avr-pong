dcf1248aa9990574b48da29fefc310866f35311f
Sebastian Schmidt <yath@yath.de>
2012-05-17 17:12:55 +0200

    Add a ball \o/

--- main.c
+++ main.c
@@ -1,20 +1,27 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "uart.h"
+#include "debug.h"
 #include "bits.h"
 #include "lcd.h"
 #include "rand.h"
 
-typedef enum {
-    DOWN_RIGHT,
-    UP_RIGHT,
-    DOWN_LEFT,
-    UP_LEFT
+#define BALL_LEFT BV(1)
+#define BALL_UP   BV(0)           /*   .-  1 = left, 0 = right */
+typedef enum {                    /*   v,- 1 = up, 0 down */
+    DOWN_RIGHT = 0,               /* 0b00 */
+    UP_RIGHT  = BALL_UP,          /* 0b01 */
+    DOWN_LEFT = BALL_LEFT,        /* 0b10 */
+    UP_LEFT   = BALL_UP|BALL_LEFT /* 0b11 */
 } direction_t;
 
+/* (0,0) is top left corner */
+typedef uint8_t ball_x_t;
+typedef uint8_t ball_y_t;
 struct {
-    uint8_t x;
-    uint8_t y;
+    ball_x_t x;
+    ball_y_t y;
     direction_t dir;
 } ball;
 
@@ -25,14 +32,28 @@ typedef uint16_t paddle_t;
 #define PADDLE_TOP_BITV BV(PADDLE_TOP_BIT)
 #define PADDLE_BOT_BITV BV(PADDLE_BOT_BIT)
 
-#define PADDLE_POS_P1 (1<<4) /* leftmost */
+#define BALL_MAX_X ((LCD_DISP_LENGTH*LCD_CHAR_WIDTH)-1)
+#define BALL_MAX_Y PADDLE_TOP_BIT
+
+#define LCD_CHAR_WIDTH 5
+#define LCD_CHAR_HEIGHT 8
+
+#if LCD_CHAR_HEIGHT != 8
+#warn "LCD_CHAR_HEIGHT is not 8. things will break."
+#endif
+
+#define PADDLE_POS_P1 (1<<(LCD_CHAR_WIDTH-1)) /* leftmost */
 #define PADDLE_POS_P2 (1)    /* rightmost */
 
 #define PADDLE_LEN 5
 #define PADDLE_LEN_BITS ((1<<PADDLE_LEN)-1)
 
+#define BALL_CGCHAR 4
+#define BALL_CGADDR (LCD_CHAR_HEIGHT*BALL_CGCHAR)
+
 /* MSB is top of screen */
 paddle_t paddle_p1, paddle_p2;
+
 
 void cgaddr(uint8_t c) {
     lcd_command(BV(LCD_CGRAM)|c);
@@ -50,10 +71,23 @@ void draw_paddles(void) {
     lcd_gotoxy(LCD_DISP_LENGTH-1, 1);
     lcd_data(3);
     cgaddr(0);
+    int bline = -1;
+    int bbit  = 0;
+    if (ball.x < LCD_CHAR_WIDTH) { /* draw ball left */
+        bline = PADDLE_TOP_BIT-ball.y;
+        bbit  = BV(LCD_CHAR_WIDTH-ball.x);
+        DEBUG("bbit line %d left: %d", bline, bbit);
+    }
     for (int i = PADDLE_TOP_BIT; i >= PADDLE_BOT_BIT; i--)
-        lcd_data(paddle_p1 & BV(i) ? PADDLE_POS_P1 : 0);
+        lcd_data((paddle_p1 & BV(i) ? PADDLE_POS_P1 : 0) | (i == bline ? bbit : 0));
+    if (ball.x > BALL_MAX_X-LCD_CHAR_WIDTH) { /* draw ball right */
+        bline = PADDLE_TOP_BIT-ball.y;
+        bbit  = BV(BALL_MAX_X-ball.x);
+        DEBUG("bbit line %d right: %d", bline, bbit);
+    } else
+        bline = -1;
     for (int i = PADDLE_TOP_BIT; i >= PADDLE_BOT_BIT; i--)
-        lcd_data(paddle_p2 & BV(i) ? PADDLE_POS_P2 : 0);
+        lcd_data((paddle_p2 & BV(i) ? PADDLE_POS_P2 : 0) | (i == bline ? bbit : 0));
 }
 
 void paddle_up(paddle_t *paddle) {
@@ -70,35 +104,163 @@ void paddle_down(paddle_t *paddle) {
     draw_paddles();
 }
 
-void init_game(void) {
+void new_game(void) {
+    DEBUG("Starting new game, random seed = 0x%02x", rand());
     /* Random paddle position */
     paddle_p1 = PADDLE_LEN_BITS <<
         (rand() % (PADDLE_TOP_BIT-PADDLE_LEN));
     paddle_p2 = paddle_p1;
     paddle_p2 = PADDLE_LEN_BITS <<
         (rand() % (PADDLE_TOP_BIT-PADDLE_LEN));
+
+    /* Somewhere in the middle of the screen */
+    ball.x = (rand() % (BALL_MAX_X-40))+20;
+    ball.y = rand() % (BALL_MAX_Y-10)+5;
+    ball.dir = rand()%4;
+
+    DEBUG("paddle_p1 = 0x%02x, paddle_p2 = 0x%02x", paddle_p1, paddle_p2);
+    DEBUG("ball.x = %d, .y = %d, .dir = %d", ball.x, ball.y, ball.dir);
+    lcd_clrscr();
+    /* clear ball */
+    cgaddr(BALL_CGADDR);
+    for (int i = 0; i < LCD_CHAR_HEIGHT; i++)
+        lcd_data(0);
+    draw_paddles();
 }
 
+void gameover(int winner) {
+    lcd_clrscr();
+    lcd_gotoxy(0, 0);
+              /*0123456789ABCDEF */
+    lcd_puts_P("   GAME OVER!");
+    lcd_gotoxy(2, 1);
+    lcd_puts_P("Player ");
+    lcd_data(winner+'0');
+    lcd_puts_P(" won");
+    _delay_ms(2000);
+    new_game();
+}
+
+int move_ball(void) {
+    int newx = ball.x;
+    int newy = ball.y;
+    int bounce = 0;
+    if (ball.dir & BALL_UP) {
+        if (--newy == 0) {
+            bounce |= BALL_UP;
+        }
+    } else {
+        if (++newy == BALL_MAX_Y) {
+            bounce |= BALL_UP;
+        }
+    }
+
+#   define ON_PADDLE(y, paddle) ((paddle) & BV(PADDLE_TOP_BIT-y))
+    if (ball.dir & BALL_LEFT) {
+        if (--newx == 0) {
+            if (ON_PADDLE(ball.y, paddle_p1) || ON_PADDLE(newy, paddle_p1)) {
+                newx++;
+                bounce |= BALL_LEFT;
+            } else {
+                gameover(2);
+                return 0;
+            }
+        }
+    } else {
+        if (++newx == BALL_MAX_X) {
+            if (ON_PADDLE(ball.y, paddle_p2) || ON_PADDLE(newy, paddle_p2)) {
+                newx--;
+                bounce |= BALL_LEFT;
+            } else {
+                gameover(1);
+                return 0;
+            }
+        }
+    }
+    DEBUG("move ball from %d,%d to %d,%d (bounce %d)", ball.x, ball.y,
+        newx, newy, bounce);
+    if (bounce)
+        ball.dir ^= bounce;
+    ball.x = newx;
+    ball.y = newy;
+
+    return 1; /* Move succeeded */
+}
+
+void move_and_draw_ball(void) {
+#   define BALL_CHARPIX(prefix, thisball) \
+        int prefix##x_c = (thisball).x / LCD_CHAR_WIDTH; \
+        int prefix##x_p = LCD_CHAR_WIDTH - ((thisball).x % LCD_CHAR_WIDTH); \
+        \
+        int prefix##y_c = (thisball).y / LCD_CHAR_HEIGHT; \
+        int prefix##y_p = (thisball).y % LCD_CHAR_HEIGHT;
+#   define IS_NEAR_TO_PADDLE(x) \
+        ((x) == 0 || (x) == LCD_DISP_LENGTH-1)
+
+    BALL_CHARPIX(old_, ball);
+    if (!move_ball())
+        return;
+    BALL_CHARPIX(new_, ball);
+
+    if (IS_NEAR_TO_PADDLE(old_x_c) || IS_NEAR_TO_PADDLE(new_x_c)) {
+        draw_paddles();
+    }
+
+    /* character boundary */
+    if ((old_x_c != new_x_c) || (old_y_c != new_y_c)) {
+        if (!IS_NEAR_TO_PADDLE(old_x_c)) {
+            lcd_gotoxy(old_x_c, old_y_c);
+            lcd_data(' ');
+        }
+
+        if (!IS_NEAR_TO_PADDLE(new_x_c)) {
+            lcd_gotoxy(new_x_c, new_y_c);
+            lcd_data(BALL_CGCHAR);
+        }
+    }
+
+    (void)old_x_p; /* unused */
+
+    if (!IS_NEAR_TO_PADDLE(old_x_c)) {
+        cgaddr(BALL_CGADDR+old_y_p);
+        lcd_data(0);
+    }
+
+    if (!IS_NEAR_TO_PADDLE(new_x_c)) {
+        cgaddr(BALL_CGADDR+new_y_p);
+        lcd_data(BV(new_x_p));
+    }
+}
+
+
 int main(void) {
+    uart_init();
+    debug_init();
     lcd_init(LCD_DISP_ON);
     rand_init();
-    init_game();
-    draw_paddles();
+
+    new_game();
+
+    int i = 0;
 
     while(1) {
-        _delay_ms(100);
-        switch (rand()%4) {
-            case 0:
-                paddle_up(&paddle_p1);
-                break;
-            case 1:
-                paddle_down(&paddle_p1);
-                break;
-            case 2:
-                paddle_up(&paddle_p2);
-                break;
-            case 3:
-                paddle_down(&paddle_p2);
+        _delay_ms(DEBUGGING ? 10 : 50);
+        move_and_draw_ball();
+        if (++i == 4) {
+            i = 0;
+            switch (rand()%4) {
+                case 0:
+                    paddle_up(&paddle_p1);
+                    break;
+                case 1:
+                    paddle_up(&paddle_p2);
+                    break;
+                case 2:
+                    paddle_down(&paddle_p1);
+                    break;
+                case 3:
+                    paddle_down(&paddle_p2);
+            }
         }
     }
 }
